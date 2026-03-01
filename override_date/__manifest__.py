85cf0c4816b3a0473856b5296d6594f14b7bccdb
Mohammad abusubhia <m.abusubhia>
2026-03-01 22:03:35 +0300

    override

---
+++ override_date/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "override_date",
+    'author': "Techs Factory",
+    'website': "https://www.techsfactory.com",
+    'version': '0.1',
+    'depends': ['base','account'],
+    'data': [
+    ],
+    'demo': [
+
+    ],
+}
