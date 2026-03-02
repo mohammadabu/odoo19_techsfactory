047481aec4565973912b1a6574b7cb6b899d3479
Mohammad abusubhia <m.abusubhia>
2026-03-03 01:09:41 +0300

    work on theme

--- auto_pay_invoices/models/auto_pay_1.py
+++ auto_pay_invoices/models/auto_pay_1.py
@@ -8,29 +8,16 @@ class AutoPay1(models.Model):
     @api.model
     def cron_auto_pay_all_invoices_1(self):
 
-        # Find all posted invoices that are not showing as paid
+        # Only posted invoices that are not showing as paid
         invoices = self.search([
             ('move_type', '=', 'out_invoice'),
             ('state', '=', 'posted'),
             ('payment_state', '!=', 'paid')
         ])
 
-        if not invoices:
-            return
-
-        # Force recompute of stored payment_state field
-        invoices.invalidate_cache(['line_ids', 'amount_residual', 'payment_state'])
-        invoices._compute_payment_state()  # recompute
-        invoices._write({'payment_state': invoices.mapped('payment_state')})
-
-        # Do the same for posted payments
-        payments = self.env['account.payment'].search([
-            ('state', '=', 'posted'),
-            ('payment_state', '!=', 'paid')
-        ])
-
-        if payments:
-            payments.invalidate_cache(['move_line_ids', 'payment_state'])
-            payments._compute_payment_state()
-            payments._write({'payment_state': payments.mapped('payment_state')})
+        for invoice in invoices:
+            # If invoice is fully paid (residual is 0), force payment_state update
+            if invoice.amount_residual <= 0:
+                # Trigger compute by writing the same value
+                invoice.write({'invoice_date': invoice.invoice_date or False})
 
