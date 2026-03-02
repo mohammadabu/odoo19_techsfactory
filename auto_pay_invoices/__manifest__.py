b28cbcef4cee8d322cdc84ff0741ce7381af09ee
Mohammad abusubhia <m.abusubhia>
2026-03-02 23:52:43 +0300

    sadsa

---
+++ auto_pay_invoices/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Auto Pay All Invoices',
+    'version': '1.0',
+    'depends': ['account'],
+    'data': [
+        'data/cron.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
