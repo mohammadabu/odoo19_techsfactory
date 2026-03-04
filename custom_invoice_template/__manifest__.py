f7a044f2c41fdc8f80e1334f3166ee31dede29a4
Mohammad abusubhia <m.abusubhia>
2026-03-04 23:38:48 +0300

    work on invoices

--- custom_invoice_template/__manifest__.py
+++ custom_invoice_template/__manifest__.py
@@ -5,9 +5,9 @@
     'version': '0.1',
     'depends': ['base','account'],
     'data': [
-        'views/templates.xml',
-        'views/custom_bank_view.xml',
-        'views/custom_invoice.xml'
+        # 'views/templates.xml',
+        # 'views/custom_bank_view.xml',
+        # 'views/custom_invoice.xml'
     ],
     'demo': [
 
