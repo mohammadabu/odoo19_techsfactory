6599cb33fe976ba99a97a3eb448ffaf88e71041b
Mohammad abusubhia <m.abusubhia>
2026-03-04 23:31:39 +0300

    work on invoices

---
+++ custom_invoice_template/models/custom_invoice.py
@@ -0,0 +1,11 @@
+import json
+import logging
+_logger = logging.getLogger(__name__)
+from odoo import models, fields, api,exceptions
+class Custom_Invoice(models.Model):
+    _inherit = 'account.move'
+    flag = fields.Char()
+
+
+    # def _check_balanced(self):
+    #     return True
